80649ab3632ae735c6de310e6543d540289bfcba
Faizhar Riskisyah <mohamad.faizhar@gmail.com>
2021-06-10 12:24:13 +0700

    patch: remove component's container

--- src/Components/Molecules/UserDetail/UserDetail.tsx
+++ src/Components/Molecules/UserDetail/UserDetail.tsx
@@ -3,22 +3,17 @@ import './UserDetail.scss';
 
 const UserDetail: React.FC = () => {
     return (
-        <div className="container">
-            <div className="user-detail">
-                <img
-                    src="https://randomuser.me/api/portraits/men/78.jpg"
-                    alt=""
-                />
-                <div className="detail">
-                    <span>Full Name</span>
-                    <span>Adam Kumis</span>
-                    <span>Telephone</span>
-                    <span>0888787878</span>
-                    <span>Email Address</span>
-                    <span>adam.kumis@tebel.com</span>
-                    <span>Country</span>
-                    <span>Jember</span>
-                </div>
+        <div className="user-detail">
+            <img src="https://randomuser.me/api/portraits/men/78.jpg" alt="" />
+            <div className="detail">
+                <span>Full Name</span>
+                <span>Adam Kumis</span>
+                <span>Telephone</span>
+                <span>0888787878</span>
+                <span>Email Address</span>
+                <span>adam.kumis@tebel.com</span>
+                <span>Country</span>
+                <span>Jember</span>
             </div>
         </div>
     );
