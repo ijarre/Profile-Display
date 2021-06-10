823c1be236809ec0c2dfe577d205b5052b06fb58
Faizhar Riskisyah <mohamad.faizhar@gmail.com>
2021-06-10 22:49:13 +0700

    feat: add loading state

--- src/features/pages/Main/Main.tsx
+++ src/features/pages/Main/Main.tsx
@@ -1,12 +1,13 @@
 import React, { useState } from 'react';
 import { useQuery } from 'react-query';
+import { Button } from 'reactstrap';
 import { UserCard, UserDetail } from '../../../Components';
 // import { UserCard, UserDetail } from '../../../Components';
 import './Main.scss';
 import { getUser } from './MainService';
 
 const Main: React.FC = () => {
-    const [page] = useState<number>(1);
+    const [page, setPage] = useState<number>(1);
     const [selectedUuid, setSelectedUuid] = useState<string>('');
     const users = useQuery(['users', page], () => getUser(page), {
         keepPreviousData: true
@@ -14,21 +15,45 @@ const Main: React.FC = () => {
     return (
         <div className="main-container p-2">
             <div className="main-cards">
-                {users.data?.results.map((user: any, i: number) => {
-                    return (
-                        <UserCard
-                            key={i}
-                            img={user.picture.large}
-                            name={`${user.name.title}.${user.name.first} ${user.name.last}`}
-                            onCardClick={() => {
-                                setSelectedUuid(user.login.uuid);
-                            }}
-                        />
-                    );
-                })}
+                {users.isLoading || users.isFetching ? (
+                    <span>Loading Data...</span>
+                ) : (
+                    users.data?.results.map((user: any, i: number) => {
+                        return (
+                            <UserCard
+                                key={i}
+                                img={user.picture.large}
+                                name={`${user.name.title}.${user.name.first} ${user.name.last}`}
+                                onCardClick={() => {
+                                    setSelectedUuid(user.login.uuid);
+                                }}
+                            />
+                        );
+                    })
+                )}
+            </div>
+            <div className="main-pagination">
+                <Button
+                    onClick={() =>
+                        setPage((old: number) => Math.max(old - 1, 0))
+                    }
+                    disabled={page === 1}
+                >
+                    {'<'}
+                </Button>
+                <span className="current-page">{page.toString()}</span>
+                <Button
+                    onClick={() => {
+                        if (!users.isPreviousData) {
+                            setPage((old) => old + 1);
+                        }
+                    }}
+                >
+                    {'>'}
+                </Button>
             </div>
             <div className="main-detail p-2">
-                {selectedUuid !== '' &&
+                {selectedUuid !== '' ? (
                     users.data?.results
                         .filter((el: any) => el.login.uuid === selectedUuid)
                         .map((user: any) => {
@@ -41,7 +66,13 @@ const Main: React.FC = () => {
                                     telephone={user.phone}
                                 />
                             );
-                        })}
+                        })
+                ) : (
+                    <span className="no-select">
+                        {' '}
+                        Click one card to display it here
+                    </span>
+                )}
             </div>
         </div>
     );
